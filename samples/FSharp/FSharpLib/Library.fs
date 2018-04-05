e9340cea61d45fc8e16a2805f23b2aeb199c2eeb
Tor Hovland <teh@powel.com>
2018-04-05 22:24:11 +0200

    Simplified reducer, possible due to the F# with keyword.

--- samples/FSharp/FSharpLib/Library.fs
+++ samples/FSharp/FSharpLib/Library.fs
@@ -25,26 +25,14 @@ type MyAppComponent() =
     inherit ReduxComponent<MyModel>()
 
 module MyFuncs =
-    let CountReducer count action =
+    let MyReducer state action =
         match action with
-            | IncrementByOne -> count + 1
-            | IncrementByValue n -> count + n
-            | _ -> count
-
-    let ForecastReducer forecasts action =
-        match action with
-            | LoadWeather -> None
-            | ReceiveWeather r -> Some r
-            | _ -> forecasts
-
-    let MainReducer state action =
-        printfn "MainReducer"
-        {
-            Count = CountReducer state.Count action;
-            Forecasts = ForecastReducer state.Forecasts action
-        }
+            | IncrementByOne -> { state with Count = state.Count + 1 }
+            | IncrementByValue n -> { state with Count = state.Count + n }
+            | LoadWeather -> { state with Forecasts = None }
+            | ReceiveWeather r -> { state with Forecasts = Some r }
 
     let reducer = Reducer<MyModel>(fun state action -> 
-        MainReducer state (action :?> MyMsg))
+        MyReducer state (action :?> MyMsg))
 
     let InitStore = new Store<_>(reducer, { Count = 0; Forecasts = None })

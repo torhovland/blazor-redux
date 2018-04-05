d22359edec3cd255472442ad692b53b085cc5ed8
Tor Hovland <teh@powel.com>
2018-04-05 18:33:35 +0200

    Removed unused code.

--- samples/BlazorStandalone/MyModel.cs
+++ samples/BlazorStandalone/MyModel.cs
@@ -1,13 +1,9 @@
 ﻿using System.Collections.Generic;
-using System.Net.Http;
-using Microsoft.AspNetCore.Blazor.Components;
 
 namespace BlazorStandalone
 {
     public class MyModel
     {
-        [Inject] public HttpClient Http { get; set; }
-
         public int Count { get; set; }
         public IEnumerable<WeatherForecast> Forecasts { get; set; }
     }

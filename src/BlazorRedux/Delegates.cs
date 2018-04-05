753ed2c469dfb67f6648e3c60f0048b03f21ce7b
Tor Hovland <teh@powel.com>
2018-04-05 18:29:26 +0200

    Moved delegates to separate file.

---
+++ src/BlazorRedux/Delegates.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorRedux
+{
+    public delegate object Dispatcher(object action);
+
+    public delegate TState Reducer<TState>(TState previousState, object action);
+
+    public delegate Task AsyncActionsCreator<in TState>(Dispatcher dispatcher, TState state);
+}
--- src/BlazorRedux/Store.cs
+++ src/BlazorRedux/Store.cs
@@ -3,12 +3,6 @@ using System.Threading.Tasks;
 
 namespace BlazorRedux
 {
-    public delegate object Dispatcher(object action);
-
-    public delegate TState Reducer<TState>(TState previousState, object action);
-
-    public delegate Task AsyncActionsCreator<in TState>(Dispatcher dispatcher, TState state);
-
     public class Store<TState>
     {
         private readonly Reducer<TState> _reducer;

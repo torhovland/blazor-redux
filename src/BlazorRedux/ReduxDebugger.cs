8a3502915783d49846466c3f23df9095f7dd6a51
Tor Hovland <teh@powel.com>
2018-04-08 11:27:09 +0200

    Flexbox.

--- src/BlazorRedux/ReduxDebugger.cs
+++ src/BlazorRedux/ReduxDebugger.cs
@@ -18,7 +18,13 @@ namespace BlazorRedux
 
                 builder.OpenElement(seq++, "style");
                 builder.AddContent(seq++, 
-@".redux-debugger__historic-entry {
+@".redux-debugger {
+    display: flex;
+    flex-wrap: wrap;
+    border: 1px solid Gainsboro;
+}
+
+.redux-debugger__historic-entry {
     background-color: WhiteSmoke;
     margin: .5em;
 }
@@ -27,6 +33,12 @@ namespace BlazorRedux
     font-weight: bold;
 }");
                 builder.CloseElement();
+
+                builder.OpenElement(seq++, "div");
+                builder.AddAttribute(seq++, "class", "redux-debugger");
+
+                builder.OpenElement(seq++, "div");
+                builder.AddAttribute(seq++, "class", "redux-debugger__action-history");
 
                 foreach (var entry in Store.History)
                 {
@@ -43,8 +55,17 @@ namespace BlazorRedux
                     builder.AddContent(seq++, entry.Time.ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"));
                     builder.CloseElement();
 
-                    builder.CloseElement();
+                    builder.CloseElement(); // historic-entry
                 }
+
+                builder.CloseElement(); // action-history
+
+                builder.OpenElement(seq++, "div");
+                builder.AddAttribute(seq++, "class", "redux-debugger__action-details");
+                builder.AddContent(seq++, "Foo");
+                builder.CloseElement(); // action-details
+
+                builder.CloseElement(); // redux-debugger
             };
         }
     }

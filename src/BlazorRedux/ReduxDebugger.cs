ce203686e3071fc5a8631df8420af76ef1d9e881
Tor Hovland <teh@powel.com>
2018-04-08 08:36:36 +0200

    Added a component that will become a time-traveling debugger.

---
+++ src/BlazorRedux/ReduxDebugger.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Blazor;
+using Microsoft.AspNetCore.Blazor.Components;
+
+namespace BlazorRedux
+{
+    public class ReduxDebugger : BlazorComponent
+    {
+        public RenderFragment Debugger;
+        
+        protected override void OnInit()
+        {
+            // ReSharper disable once RedundantAssignment
+            Debugger = builder =>
+            {
+                var seq = 0;
+                builder.OpenElement(seq++, "div");
+                builder.AddContent(seq++, "Foo");
+                builder.CloseElement();
+            };
+        }
+    }
+}

ac465660ae5fb144011e13bb5f91ab6f1b91a3b4
Tor Hovland <teh@powel.com>
2018-04-08 11:14:36 +0200

    Debugger styling.

--- src/BlazorRedux/ReduxDebugger.cs
+++ src/BlazorRedux/ReduxDebugger.cs
@@ -16,14 +16,33 @@ namespace BlazorRedux
             {
                 var seq = 0;
 
+                builder.OpenElement(seq++, "style");
+                builder.AddContent(seq++, 
+@".redux-debugger__historic-entry {
+    background-color: WhiteSmoke;
+    margin: .5em;
+}
+
+.redux-debugger__historic-entry__action {
+    font-weight: bold;
+}");
+                builder.CloseElement();
+
                 foreach (var entry in Store.History)
                 {
                     builder.OpenElement(seq++, "div");
-                    builder.AddContent(seq++, entry.Time.ToString(CultureInfo.InvariantCulture));
+                    builder.AddAttribute(seq++, "class", "redux-debugger__historic-entry");
+
+                    builder.OpenElement(seq++, "div");
+                    builder.AddAttribute(seq++, "class", "redux-debugger__historic-entry__action");
+                    builder.AddContent(seq++, entry.Action?.ToString() ?? "Initial state");
                     builder.CloseElement();
 
                     builder.OpenElement(seq++, "div");
-                    builder.AddContent(seq++, entry.Action?.ToString() ?? "Initial state");
+                    builder.AddAttribute(seq++, "class", "redux-debugger__historic-entry__time");
+                    builder.AddContent(seq++, entry.Time.ToUniversalTime().ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fff'Z'"));
+                    builder.CloseElement();
+
                     builder.CloseElement();
                 }
             };

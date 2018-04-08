689b017e04db8222009a6a8c802802a1fd415be5
Tor Hovland <teh@powel.com>
2018-04-08 12:05:33 +0200

    Can select log entries in the debugger.

--- src/BlazorRedux/ReduxDebugger.cs
+++ src/BlazorRedux/ReduxDebugger.cs
@@ -1,10 +1,11 @@
-﻿using System.Globalization;
-using Microsoft.AspNetCore.Blazor;
+﻿using Microsoft.AspNetCore.Blazor;
 
 namespace BlazorRedux
 {
     public class ReduxDebugger<TModel, TAction> : ReduxComponent<TModel, TAction>
     {
+        private HistoricEntry<TModel, TAction> _selectedEntry;
+
         public RenderFragment Debugger;
         
         protected override void OnInit()
@@ -21,16 +22,30 @@ namespace BlazorRedux
 @".redux-debugger {
     display: flex;
     flex-wrap: wrap;
-    border: 1px solid Gainsboro;
+    border: 1px solid #9d9d9d;
 }
 
 .redux-debugger__historic-entry {
     background-color: WhiteSmoke;
-    margin: .5em;
+    padding: .5em;
+}
+
+.redux-debugger__historic-entry:hover { 
+    background-color: Gainsboro; 
+}
+
+.redux-debugger__historic-entry--selected, 
+.redux-debugger__historic-entry--selected:hover { 
+    background-color: #4189C7; 
+    color: White;
 }
 
 .redux-debugger__historic-entry__action {
     font-weight: bold;
+}
+
+.redux-debugger__action-details {
+    padding: .5em;
 }");
                 builder.CloseElement();
 
@@ -43,7 +58,8 @@ namespace BlazorRedux
                 foreach (var entry in Store.History)
                 {
                     builder.OpenElement(seq++, "div");
-                    builder.AddAttribute(seq++, "class", "redux-debugger__historic-entry");
+                    builder.AddAttribute(seq++, "class", "redux-debugger__historic-entry " + (entry == _selectedEntry ? "redux-debugger__historic-entry--selected" : ""));
+                    builder.AddAttribute(seq++, onclick(() => SelectEntry(entry)));
 
                     builder.OpenElement(seq++, "div");
                     builder.AddAttribute(seq++, "class", "redux-debugger__historic-entry__action");
@@ -62,11 +78,17 @@ namespace BlazorRedux
 
                 builder.OpenElement(seq++, "div");
                 builder.AddAttribute(seq++, "class", "redux-debugger__action-details");
-                builder.AddContent(seq++, "Foo");
+                builder.AddContent(seq++, _selectedEntry?.Action?.ToString() ?? "No action selected.");
                 builder.CloseElement(); // action-details
 
                 builder.CloseElement(); // redux-debugger
             };
+        }
+
+        void SelectEntry(HistoricEntry<TModel, TAction> entry)
+        {
+            _selectedEntry = entry;
+            StateHasChanged();
         }
     }
 }

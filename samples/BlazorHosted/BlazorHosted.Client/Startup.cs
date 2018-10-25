a8fc9d48342d57b54650f892c5c5b18e5e071807
Sergey Solin <ssolin@centeredgesoftware.com>
2018-10-25 15:00:31 +0300

    Update to able run with Blazor version 0.6.0
    * Upgraded nuget packages:
      * Microsoft.AspNetCore.Blazor.Browser => 0.6.0
      * Microsoft.AspNetCore.Blazor.Build => 0.6.0
      * Microsoft.AspNetCore.Blazor.Server => 0.6.0
      * Microsoft.AspNetCore.Blazor.Cli => 0.6.0
      * Microsoft.AspNetCore.Razor.Design => 2.1.2
    * Converted Client project to use new startup template (using
    Startup.cs file)
    * Removed ILayoutComponent dependencies, replaced with
    BlazorLayoutComponent
    * Updated js code running logic to use Microsoft.JSInterop
    * Replaced legacy JsonUtil with Json from Microsoft.JSInterop

---
+++ samples/BlazorHosted/BlazorHosted.Client/Startup.cs
@@ -0,0 +1,27 @@
+﻿using BlazorRedux;
+using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorHosted.Client
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddReduxStore<MyState, IAction>(
+                    new MyState(), Reducers.RootReducer, options =>
+                    {
+                        options.GetLocation = state => state.Location;
+                    });            
+        }
+
+        public void Configure(IBlazorApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}

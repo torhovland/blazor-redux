0a9cfd825509357cc7fff03ee70eb36414517b4b
David Nordvall <david.nordvall@doitsolutions.se>
2019-04-04 14:15:43 +0200

    Further update-to-Blazor-0.9.0 migration fixes.

--- samples/BlazorHosted/BlazorHosted.Server/Startup.cs
+++ samples/BlazorHosted/BlazorHosted.Server/Startup.cs
@@ -1,11 +1,12 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.AspNetCore.Blazor.Server;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
 using System.Linq;
 using System.Net.Mime;
@@ -18,10 +19,7 @@ namespace BlazorHosted.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddJsonOptions(options =>
-            {
-                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
-            });
+            services.AddMvc().AddNewtonsoftJson();
 
             services.AddResponseCompression(options =>
             {
@@ -34,7 +32,7 @@ namespace BlazorHosted.Server
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseResponseCompression();
 
